d62b34e784fbd56e70378c0ab85bc5faaa289b36
Timo Wacke <wacketimo@gmail.com>
2024-02-11 20:13:38 +0100

    final

--- resample_commits.py
+++ resample_commits.py
@@ -13,6 +13,7 @@ def parse_commits(repo_path, interval_units):
     # Run git log command to get commit information
     git_log_output = subprocess.check_output(['git', 'log', '--pretty=format:%H|%ct|%an|%ae|%s'], cwd=repo_path, universal_newlines=True)
 
+    
     # Iterate over each line of git log output
     for line in git_log_output.strip().split('\n'):
         commit_id, commit_time, author_name, author_email, commit_message = line.split('|')
@@ -71,13 +72,13 @@ if __name__ == "__main__":
     interval = int(input("How many minutes between commits to summarize? "))
     
     # make a temporary directory and clone the repository
+    temp_source_dir = mkdtemp()    
     temp_dir = mkdtemp()
-    
 
     # get the list of desired commits from the source branch
-    subprocess.run(["git", "clone", repo_url, temp_dir])
-    subprocess.run(["git", "checkout", source_branch], cwd=temp_dir)
-    desired_commits = parse_commits(temp_dir, interval)
+    subprocess.run(["git", "clone", repo_url, temp_source_dir])
+    subprocess.run(["git", "checkout", source_branch], cwd=temp_source_dir)
+    desired_commits = parse_commits(temp_source_dir, interval)
    
     # connect in the destination directory to the origin and create a new clean empty branch
     # git init with origin
@@ -85,20 +86,25 @@ if __name__ == "__main__":
     subprocess.run(["git", "config", "user.email",  commiter_email], cwd=temp_dir)
     subprocess.run(["git", "config", "user.name",  commiter_name], cwd=temp_dir)
     subprocess.run(["git", "remote", "add", "origin", repo_url], cwd=temp_dir)
-    subprocess.run(["git", "switch", "--orphan", new_branch_name], cwd=temp_dir)
+    subprocess.run(["git", "checkout", "-b", new_branch_name], cwd=temp_dir)
     
-    
-    # for each commit in the list of desired commits, cherry-pick the commit from the source branch
+    # for each commit in the list of desired commits checkout the source branch and copy it to the destination directory
     for commit_group in desired_commits:
-        commit = commit_group.get("lead_commit")
-        # cherry-pick the commits from the source branch
-        subprocess.run(["git", "cherry-pick"] + [commit_id for commit_id in commit_group.get("summarizing_commit_ids")], cwd=temp_dir)
+        subprocess.run(["git", "checkout", commit_group["lead_commit"]["commit_id"]], cwd=temp_source_dir)
+        # copy the files from the source directory to the destination directory
+        # ignore the .git directory
+        subprocess.run(["rsync", "-a", "--exclude", ".git", "--delete", temp_source_dir + "/", temp_dir], cwd=temp_dir)
         subprocess.run(["git", "add", "."], cwd=temp_dir)
-        # commit the changes to the destination directory
-        commit_time = datetime.datetime.utcfromtimestamp(commit["commit_time"])
-        author_email = commit["author_info"]["email"]
-        author_name = commit["author_info"]["name"]
-        message = commit["message"]
-        subprocess.run(["git", "commit", "--date", commit_time.strftime('%Y-%m-%d %H:%M:%S'), "--author", f"{author_name} <{author_email}>", "-m", message], cwd=temp_dir)
+        # commit the changes
+        # time format Wed Feb 16 14:00 2011 +0100
+        args = ["git", "commit", "-m", commit_group["lead_commit"]["message"], f'--date={datetime.datetime.fromtimestamp(commit_group["lead_commit"]["commit_time"]).strftime("%a %b %d %H:%M %Y %z")}']
+        subprocess.run(args, cwd=temp_dir)
+        print(args)
         
-    subprocess.run(["git", "push", "origin", new_branch_name], cwd=temp_dir)
+    # git filter-branch --env-filter 'export GIT_COMMITTER_DATE="$GIT_AUTHOR_DATE"'
+    subprocess.run(["git", "filter-branch", "--env-filter", 'export GIT_COMMITTER_DATE="$GIT_AUTHOR_DATE"'], cwd=temp_dir)
+        
+    subprocess.run(["git", "push", "--force", "origin", new_branch_name], cwd=temp_dir)
+    
+    shutil.rmtree(temp_source_dir)
+    shutil.rmtree(temp_dir)
